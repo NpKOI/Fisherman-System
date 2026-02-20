71cf7d0e1207a738314eed411cab6bedba5d551c
Атила Таир <tojitiktok@proton.me>
2026-02-20 01:14:24 -0800

    Initial Commit 2.0

---
+++ FisherMan.ASP/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FisherMan.ASP.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
